933d59e13bcbc4f4fc50e88edd89f83c99ba333f
Micah Redding <micahtredding@gmail.com>
2025-09-21 18:27:23 -0500

    Update community rss feeds

--- src/_data/community.js
+++ src/_data/community.js
@@ -1,25 +1,27 @@
 const Parser = require('rss-parser');
+const fs = require('fs');
+const path = require('path');
 let parser = new Parser();
 
 module.exports = async function() {
-    const feedUrls = [
-        // 'https://members.christiantranshumanism.org/blog/community.rss',
-        // 'http://jaygary.com/feed/',
-        // 'http://mikemorrell.org/feed',
-        // 'https://turingchurch.net/feed',
-        // 'https://www.aitheology.com/feed/',
-        'http://feeds.metacannon.net/LincolnCannon',
-        'http://glassdimly.com/rss.xml',
-        'http://www.blaireostler.com/journal?format=RSS',
-        'http://www.christopherbenek.com/?feed=rss2',
-        'https://josiahredding.com/feed/',
-        'https://ryanhauck1.wordpress.com/rss',
-        'https://thesingularity.com/?feed=rss',
-        'https://www.micahredding.com/blog.rss',
-        'https://www.turingchurch.com/feed',
-        'https://anchor.fm/s/d5e30910/podcast/rss',
-        'https://manyworlds2017.wordpress.com/feed/'
-    ];
+    // Feeds that have died or many no longer be relevant
+        // "https://members.christiantranshumanism.org/blog/community.rss",
+        // "http://jaygary.com/feed/",
+        // "http://mikemorrell.org/feed",
+        // "https://turingchurch.net/feed",
+        // "https://www.aitheology.com/feed/",
+        // "http://www.blaireostler.com/journal?format=RSS",
+        // "https://anchor.fm/s/d5e30910/podcast/rss",
+        // "http://rundown.live/rss",
+        // "http://bibleandprayer.com/rss",
+        // "http://feeds.feedburner.com/Exotheologyorg",
+        // "https://artoonsolutions.com/feed/",
+        // "https://successdunia.com/feed/",
+        // "https://theanglicangazette.podbean.com/feed.xml",
+
+    // Read feed URLs from the JSON file
+    const feedsPath = path.join(__dirname, 'community-feeds.json');
+    const feedUrls = JSON.parse(fs.readFileSync(feedsPath, 'utf8'));
 
     const feeds = await Promise.allSettled(
         feedUrls.map(url => 
